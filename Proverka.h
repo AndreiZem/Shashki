5a7ca39502d53a9fd8c40a6d873b60dd18939959
Змачинский Андрей <89727842+AndreiZem@users.noreply.github.com>
2021-10-21 00:03:16 +0300

    Add files via upload

---
+++ Proverka.h
@@ -0,0 +1,12 @@
+#ifndef PROVERKA_H
+#define PROVERKA_H
+#include "main.h"
+
+// extern int saska,damka,sopsas,sopdam;
+int Proverka(Doska,int);
+int ProverkaBelyh(Doska);
+int ProverkaChernyh(Doska);
+int HodBelyh(Doska);
+int HodChernyh(Doska);
+
+#endif // PROVERKA_H
